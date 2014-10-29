97863e0da34e7a96c52d2f3e02d0bf0146d73ade
jecelyin <jecelyin@gmail.com>
2014-10-29 18:20:19 +0800

    support c2d 3.3

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,9 +1,8 @@
-APP_STL := c++_static
-NDK_TOOLCHAIN_VERSION=clang
+# 确保NDK版本是r9以上
+APP_STL := gnustl_static
 
-APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -fexceptions -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -fsigned-char -std=gnu++11 -pthread # -std=c++11
 APP_LDFLAGS := -latomic
-
 
 APP_DEBUG := $(strip $(NDK_DEBUG))
 ifeq ($(APP_DEBUG),1)
